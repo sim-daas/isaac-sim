186a550712ac62a1472a7c4caa65740443dd6234
sim-daas <aitechmanml@gmail.com>
2026-02-03 23:00:15 +0530

    demos

--- test/robo_base.py
+++ test/robo_base.py
@@ -28,19 +28,16 @@ from isaacsim.core.prims import Articulation
 from isaacsim.core.utils.types import ArticulationAction
 from isaacsim.core.utils.stage import add_reference_to_stage, get_stage_units
 from isaacsim.core.api.robots import Robot
+from isaacsim.storage.native import get_assets_root_path
 
 # Scene setup
 world = World(stage_units_in_meters=1.0)
 world.scene.add_default_ground_plane()
 
-import os 
-home_path = os.path.expanduser("~")
-asset_path = os.path.join(home_path, "gitrepos/isaac-sim/test")
-robo_path = asset_path + "/limo.usd"
-print(robo_path)
-add_reference_to_stage(usd_path=robo_path, prim_path="/World/limo")
-car = world.scene.add(Robot(prim_path="/World/limo", name="car"))
-car_controller = car.get_articulation_controller()
+assets_root_path = get_assets_root_path()
+asset_path = assets_root_path + "/Isaac/Robots/NVIDIA/NovaCarter/nova_carter.usd"
+add_reference_to_stage(usd_path=asset_path, prim_path="/World/Car")
+car = world.scene.add(Robot(prim_path="/World/Car", name="my_car"))
 
 world.reset()
 optimise()
@@ -50,7 +47,7 @@ start_time= time.time()
 while simulation_app.is_running():
     world.step(render=True)
     frame_count = printfps(frame_count, start_time)
-    car_controller.apply_action(ArticulationAction(joint_positions=None, joint_efforts=None, joint_velocities=[6, 1, 6, 1]))
+    car.apply_action(ArticulationAction(joint_positions=None, joint_efforts=None, joint_velocities=[6, 6, 0, 0, 0, 0, 0]))
 
 
 # closing the app
